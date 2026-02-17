9c336c5bf8aee208a993dbc2fd2186655b023fca
aislinnmh <60907095+aislinnmh@users.noreply.github.com>
2026-02-16 21:52:09 -0500

    Finish calibration
    Only missing averaging and background subtraction

--- color_calibration.m
+++ color_calibration.m
@@ -176,12 +176,109 @@ for i = 1:12
     RGB_grays(i, :) = mean(reshape(roi, [], 3), 1); % Calculate mean RGB values
 end
 
-%%
-% chart.ColorROIs.ROI(1) = ROIs_color(1);
+RGB = [RGB_colors; RGB_grays];
 
-%%
-% chart.RegistrationPoints = [[x_br y_br],[x_bl y_bl],[x_tl y_tl],[x_tr y_tr]];
+%% True sRGB at D50
 
-% chart.ColorROIs.ROI = [15,17,17,17]
-% %%
-% [colorValues,colorCorrectionMatrix] = measureColor(chart)
+true_RGB_colors = csvread('gertagmacbeth.csv');
+n_grays = 12;
+interval = 255/(n_grays-1);
+true_RGB_grays = zeros(n_grays,3);
+for i = 0:n_grays-1
+    val = 255 - (i*interval);
+    true_RGB_grays(i+1,:) = [val val val];
+end
+
+true_RGB = [true_RGB_colors; true_RGB_grays];
+
+%% Matrix math
+
+% rgb * CCM = truergb
+
+CCM = RGB \ true_RGB;
+
+%% Display corrected target 
+
+img_col = double(reshape(img,xlen*ylen,3));
+img_col_corr = img_col*CCM;
+img_corr = reshape(img_col_corr,ylen,xlen,3);
+figure(4)
+subplot(1,2,1)
+imshow(img_corr/255)
+title('Color Corrected Image')
+subplot(1,2,2)
+imshow(img)
+title('Original Image')
+
+
+%% Display generated Gretag Macbeth
+
+gt_img = uint8(zeros(ylen,xlen,3));
+
+% Fill color
+xind = 6;
+yind = 1;
+side = 'top';
+sz = 50;
+for i = 1:18
+    cenx = int16(gridPos(1,yind,xind));
+    ceny = int16(gridPos(2,yind,xind));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 1) = uint8(true_RGB(i,1));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 2) = uint8(true_RGB(i,2));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 3) = uint8(true_RGB(i,3));
+
+    % Determine side
+    if side == "top" & xind == 1
+        side = "left"; % Change direction to left for the next ROI
+    elseif side == "left" && yind == 5
+        side = "bottom"; % Change direction to bottom
+    elseif side == "bottom" && xind == 6
+        side = "right"; % Change direction to right
+    elseif side == "right" && yind == 1
+        side = "top"; % Change direction back to top
+    end
+
+    % Move
+    if side == "top"
+        xind = xind - 1;
+    elseif side == "left"
+        yind = yind + 1;
+    elseif side == "bottom"
+        xind = xind + 1;
+    elseif side == "right"
+        yind = yind -1;
+    end
+end
+
+% Fill gray
+xind = 5;
+yind = 2;
+n_row = 1;
+for i = 19:30
+    cenx = int16(gridPos(1,yind,xind));
+    ceny = int16(gridPos(2,yind,xind));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 1) = uint8(true_RGB(i,1));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 2) = uint8(true_RGB(i,2));
+    gt_img(ceny-sz:ceny+sz, cenx-sz:cenx+sz, 3) = uint8(true_RGB(i,3));
+    % Move
+    if xind ~= 2
+        xind = xind - 1;
+    else
+        xind = 5;
+        yind = yind + 1;
+    end
+end
+
+figure(5)
+subplot(1,2,1)
+imshow(img_corr/255)
+title('Color Corrected Image')
+subplot(1,2,2)
+imshow(gt_img)
+title('Simulated Ground Truth')
+
+
+%% Save CCM
+
+% Save CCM in a csv file
+csvwrite("CCM.csv",CCM);
